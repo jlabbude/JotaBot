614b029d7d219f325e38da212c5ebfd60661dd03
BuildTools <unconfigured@null.spigotmc.org>
2024-01-05 00:34:44 -0300

    Optmize imports

--- src/main/java/com/github/jlabbude/jotabot/command/SlashCommand.java
+++ src/main/java/com/github/jlabbude/jotabot/command/SlashCommand.java
@@ -3,11 +3,6 @@ package com.github.jlabbude.jotabot.command;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import reactor.core.publisher.Mono;
 
-/**
- * A simple interface defining our slash command class contract.
- *  a getName() method to provide the case-sensitive name of the command.
- *  and a handle() method which will house all the logic for processing each command.
- */
 public interface SlashCommand {
     Mono<Void> execute(String commandName, MessageCreateEvent event);
 }
