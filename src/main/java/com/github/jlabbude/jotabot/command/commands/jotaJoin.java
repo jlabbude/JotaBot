faa730db2cb9619e83fe91d2c0e35635ce64b61b
Lucas <jlabbude@hotmail.com>
2024-01-07 15:35:05 -0300

    Added code to check time it took user to screenshare

--- src/main/java/com/github/jlabbude/jotabot/command/commands/jotaJoin.java
+++ src/main/java/com/github/jlabbude/jotabot/command/commands/jotaJoin.java
@@ -5,8 +5,10 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.VoiceState;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.VoiceChannel;
+import org.apache.commons.lang3.time.StopWatch;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class jotaJoin implements SlashCommand {
 
@@ -18,6 +20,10 @@ public class jotaJoin implements SlashCommand {
 
     @Override
     public Mono<Void> execute(String commandName, MessageCreateEvent event) {
+
+
+        AtomicReference<StopWatch> jotatimer = new AtomicReference<>();
+
         Message message = event.getMessage();
 
         return Mono.justOrEmpty(message.getUserMentions().stream().findFirst())
@@ -25,11 +31,30 @@ public class jotaJoin implements SlashCommand {
                         .flatMap(guild -> guild.getMemberById(user.getId()))
                         .flatMap(Member::getVoiceState)
                         .flatMap(VoiceState::getChannel)
-                        .flatMap(VoiceChannel::join)
+                        .flatMap(voiceChannel -> {
+                            jotatimer.set(StopWatch.createStarted());
+                            return voiceChannel.join();
+                        })
                 )
                 .flatMap(voiceConnection -> streamCommand.execute("streamCommand", event)
+                        .then(Mono.fromRunnable(() -> {
+
+                            jotatimer.get().stop();
+
+                            String elapsedString = jotatimer.get().toString();
+
+                            message.getChannel()
+                                .flatMap(channel -> channel.createMessage(elapsedString))
+                                .subscribe();
+
+                            System.out.println("Elapsed time: " + elapsedString);
+                        }
+                        )
+                        )
+
                         .then(voiceConnection.disconnect())
                         .then()
+
                 );
     }
 }
