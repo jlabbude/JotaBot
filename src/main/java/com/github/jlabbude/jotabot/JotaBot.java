4d40e782c7f0ce1cfdeea923c52c626db038f88d
Lucas <jlabbude@hotmail.com>
2024-01-07 15:33:22 -0300

    Added Twitter API for future use

--- src/main/java/com/github/jlabbude/jotabot/JotaBot.java
+++ src/main/java/com/github/jlabbude/jotabot/JotaBot.java
@@ -5,10 +5,17 @@ import com.github.jlabbude.jotabot.command.SlashCommandListener;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import java.util.HashSet;
+import java.util.Set;
+import com.twitter.clientlib.TwitterCredentialsOAuth2;
+import com.twitter.clientlib.ApiException;
+import com.twitter.clientlib.api.TwitterApi;
+import com.twitter.clientlib.model.*;
 
 public class JotaBot {
 
     public static void main(String[] args) {
+
         CommandManager commandManager = new CommandManager();
 
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build()
