28ab0f3fcdd5e7f1176c47634d1bd9e4291bbd79
BuildTools <unconfigured@null.spigotmc.org>
2024-01-07 00:56:19 -0300

    Changed class name to avoid confusion

--- src/main/java/com/github/jlabbude/jotabot/command/commands/jotaGrito.java
+++ src/main/java/com/github/jlabbude/jotabot/command/commands/jotaGrito.java
@@ -4,7 +4,7 @@ import com.github.jlabbude.jotabot.command.SlashCommand;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import reactor.core.publisher.Mono;
 
-public class jotaScream implements SlashCommand {
+public class jotaGrito implements SlashCommand {
     @Override
     public Mono<Void> execute(String commandName, MessageCreateEvent event) {
 
